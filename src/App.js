1ee8b1734ce161c578360d4627438ca457f6c419
Werner Buys <Werner.Buys@standardbank.co.za>
2020-10-01 14:14:39 +0200

    initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
 import React from 'react';
-import logo from './logo.svg';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import EventImage from './A-Galaxy-Event_Bangkok_main_1.jpg'
 import './App.css';
 
+const useStyles = makeStyles((theme) => ({
+  fullPageHeight: {
+    height: '100%'
+  },
+  paper: {
+    backgroundImage: `url(${EventImage})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    height: '100%'
+  },
+  heading: {
+    color: 'white',
+    flex: '0 1 auto'
+  },
+  centerContent: {
+    paddingLeft: theme.spacing(8),
+    flex: '1 1 auto'
+  },
+  joinButtonSize: {
+    width: '25%'
+  },
+  contentContainer: {
+    height: '100%',
+    padding: theme.spacing(3)
+  },
+  footerContent: {
+    color: 'white',
+    paddingLeft: theme.spacing(8),
+  }
+}));
+
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid container spacing={0} className={classes.fullPageHeight}>
+      <Grid container item xs={8} spacing={0}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper} >
+            <Grid container spacing={0} direction="column" className={classes.contentContainer}>
+              <Grid item className={classes.heading}>
+                <Typography variant="h3">
+                  GDG Group
+                </Typography>
+              </Grid>
+              <Grid container spacing={0} item direction="column" justify="center" className={classes.centerContent}>
+                <Typography variant="h2">
+                  GDG Group
+                </Typography>
+                <Typography variant="body1" gutterBottom>
+                  Description of the event and some small information
+                </Typography>
+                <Button variant="contained" color="secondary" className={classes.joinButtonSize}>
+                  Secondary
+                </Button>
+              </Grid>
+              <Grid item className={classes.footerContent}>
+                <Typography variant="h3">
+                  GDG Group
+                </Typography>
+              </Grid>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
+      <Grid container item xs={4} spacing={0}>
+        <Grid container item xs={12} spacing={0}>
+          <Grid item xs={12}>1</Grid>
+          <Grid item xs={12}>1</Grid>
+          <Grid item xs={12}>1</Grid>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 }
 
